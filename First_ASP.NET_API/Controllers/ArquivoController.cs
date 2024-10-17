d4d929df262c3974e2f35c6341aa4b30c6854e25
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-10-17 13:18:42 -0300

    Create GerenciaArquivo, ArquivoController

---
+++ First_ASP.NET_API/Controllers/ArquivoController.cs
@@ -0,0 +1,33 @@
+﻿using First_ASP.NET_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace First_ASP.NET_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArquivoController : ControllerBase
+    {
+        [HttpGet("lerArquivo")]
+        public IActionResult LerArquivo()
+        {
+            // Defina o caminho absoluto manualmente
+            var caminhoArquivo = @"C:\Users\aliss\source\repos\First_ASP.NET_API\First_ASP.NET_API\bin\Debug\net8.0\Arquivos\resumoHobbit.txt";
+
+            var (sucesso, linhasArquivo, quantidadeLinhas) = GerenciaArquivo.LerArquivo(caminhoArquivo);
+
+            if (sucesso)
+            {
+                // Formatação da resposta corrigida
+                var resposta = $"Quantidade de linhas do arquivo: {quantidadeLinhas}\n" +
+                               $"Arquivo: {string.Join("\n", linhasArquivo)}";
+
+                return Ok(resposta); // Removido o ponto e vírgula incorreto
+            }
+            else
+            {
+                return BadRequest("Erro ao ler o arquivo.");
+            }
+        }
+    }
+}
---
+++ First_ASP.NET_API/Model/GerenciaArquivo.cs
@@ -0,0 +1,22 @@
+﻿namespace First_ASP.NET_API.Model
+{
+    public class GerenciaArquivo
+    {
+        public static (bool Sucesso, string[] Linhas, int QuantidadeLinhas) LerArquivo(string caminhoArquivo)
+        {
+
+            try
+            {
+                string[] linhas = File.ReadAllLines(caminhoArquivo);
+                return(true, linhas, linhas.Count());
+            }
+            catch (Exception ex)
+            {
+                return (false, new string[0], 0);
+            }
+
+        }
+
+
+    }
+}
